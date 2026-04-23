Updated input file and comparison file with correct units. Not currently working properly

test/tests/tungsten_H_diffusion/comparison_ver-1dd.py
@@ -10,9 +10,31 @@ os.chdir(script_folder)
 
 num_summation_terms = 2000
 N_o = 3.1622e13  # (atom/mm^3) For convenience lattice density chosen as 3.1622e22 atom/m^3
-c_o = 0.0001  # dissolved gas atom fraction (-)
-D = 1.632  # diffusivity (mm^2/min) ---- 1.632 m^2/min, but input as if s. Corrected before graphing.
+# c_o = 0.0001  # dissolved gas atom fraction (-)
+# D = 1.632  # diffusivity (mm^2/min) ---- 1.632 m^2/min, but input as if s. Corrected before graphing.
+# l = 0.025  # slab thickness (mm) --- 0.025 mm
+
+######################################### New Stuff
+
+lambdaa = 3.1622e-8 * 1000  # lattice parameter (mm) || lambda is a python keyword (still TMAP8 default)
+nu = 1e13 * 60  # Debye frequency (1/min)
+rho = 0.01  # trapping site fraction
+D_o = 7.44e-08 * 1000**2 * 60  # diffusivity pre-exponential (mm^2/min)
+Ed = 0.13  # diffusion activation energy (eV)
+
+k = 8.61733326e-5 # Boltzmann's constant(eV/K)
+T = 1500  # temperature (K)
+epsilon_k_ratio = 9864
+epsilon = k * epsilon_k_ratio  # epsilon: trap energy
+c = 0.0001  # dissolved gas atom fraction
+zeta = ((lambdaa**2) * nu * np.exp((Ed - epsilon) / (k * T)) / (rho * D_o)) + (c / rho)
+
+D = 1.632  # diffusivity (mm^2/min)
+D_eff = D / (1 + (1 / zeta))  # Effective diffusivity (mm^2/min)
 l = 0.025  # slab thickness (mm) --- 0.025 mm
+c_o = c
+tau_be = l**2 / (2 * (np.pi) ** 2 * D_eff)
+
 
 # Extract data from 'gold' TMAP8 run
 # if "/tmap8/doc/" in script_folder.lower():  # if in documentation folder
@@ -20,7 +42,7 @@ l = 0.025  # slab thickness (mm) --- 0.025 mm
 # else:  # if in test folder
 #     csv_folder = "./gold/ver-1dd_out.csv"
 # csv_folder = "./ver-1dd_out.csv"
-csv_folder = "./ver-1dd_out.csv"
+csv_folder = "./ver-1dd_trapping_out.csv"
 tmap_sol = pd.read_csv(csv_folder)
 tmap_time = np.array(tmap_sol["time"]) # minutes
 tmap_prediction = np.array(tmap_sol["scaled_outflux"])
@@ -28,23 +50,23 @@ In_minus_Out = np.array(tmap_sol["time_integrated_flux"])
 Accumulation = np.array(tmap_sol["mass_in_domain"])
 
 
-idx = np.where(tmap_time >= 0.0000001)[0][0]
+idx = np.where(tmap_time >= 0.0001)[0][0]
 
-# Calculate the breakthrough time from numerical solution
-tmap_slope = (tmap_prediction[idx + 1 :] - tmap_prediction[idx:-1]) / (
-    tmap_time[idx + 1 :] - tmap_time[idx:-1]
-)
-tmap_intercept = tmap_time[np.argmax(tmap_slope) + idx] - (
-    tmap_prediction[int(np.argmax(tmap_slope) + idx)]
-) / np.max(tmap_slope)
-output_line = f"The breakthrough time from numerical solution is {tmap_intercept:.2e} s"
-print(output_line)
+# # Calculate the breakthrough time from numerical solution
+# tmap_slope = (tmap_prediction[idx + 1 :] - tmap_prediction[idx:-1]) / (
+#     tmap_time[idx + 1 :] - tmap_time[idx:-1]
+# )
+# tmap_intercept = tmap_time[np.argmax(tmap_slope) + idx] - (
+#     tmap_prediction[int(np.argmax(tmap_slope) + idx)]
+# ) / np.max(tmap_slope)
+# output_line = f"The breakthrough time from numerical solution is {tmap_intercept:.2e} s"
+# print(output_line)
 
 # analytical solution
-analytical_time = np.array(tmap_time)
-tau_be = l**2 / (2 * (np.pi) ** 2 * D)  # calculate analytical breakthrough time
-output_line = f"The breakthrough time from analytical solution is {tau_be:.2e} s"
-print(output_line)
+analytical_time = tmap_time
+# tau_be = l**2 / (2 * (np.pi) ** 2 * D)  # calculate analytical breakthrough time
+# output_line = f"The breakthrough time from analytical solution is {tau_be:.2e} s"
+# print(output_line)
 
 
 def summation_term(num_terms, time):
@@ -55,6 +77,7 @@ def summation_term(num_terms, time):
 
 
 # Calculate the analytical solution
+c_o = c
 analytical_flux = (
     N_o * (c_o * D / l) * (1 + 2 * summation_term(num_summation_terms, analytical_time))
 )
@@ -78,22 +101,22 @@ ax.plot(
 #     label=r"Numerical breakthrough time",
 #     c="tab:brown",
 # )
-ax.plot(
-    [tau_be, tau_be],
-    [0, 2.0e11],
-    label=r"Analytical breakthrough time",
-    c="tab:brown",
-    linestyle="--",
-)
+# ax.plot(
+#     [tau_be, tau_be],
+#     [0, 2.0e11],
+#     label=r"Analytical breakthrough time",
+#     c="tab:brown",
+#     linestyle="--",
+# )
 ax.set_xlabel("Time (s)")
-ax.set_ylabel("Flux (atom/mm$^2$s)") # For tungsten in atom/mm^2, for TZM atom/mm^2
+ax.set_ylabel("Flux (atom/mm$^2$s)") # For tungsten in atom/mm^2, or TZM atom/mm^2
 ax.legend(loc="best")
 # ax.set_xlim(left=0, right=1.5)
 ax.set_ylim(bottom=0)
 plt.grid(visible=True, which="major", color="0.65", linestyle="--", alpha=0.3)
 RMSE = np.sqrt(np.mean((tmap_prediction - analytical_flux)[idx:] ** 2))
 RMSPE = RMSE * 100 / np.mean(analytical_flux[idx:])
-ax.text(0.015, 1.5e11, "RMSPE = %.2f " % RMSPE + "%", fontweight="bold")
+# ax.text(0.015, 1.5e11, "RMSPE = %.2f " % RMSPE + "%", fontweight="bold")
 # ax.text(
 #     0.25e-5,
 #     0.5e14,
@@ -119,7 +142,7 @@ plt.plot(tmap_time, In_minus_Out, label = 'In - Out')
 RMSE = np.sqrt(np.mean((Accumulation-In_minus_Out)**2) )
 RMSPE = RMSE*100/np.mean(In_minus_Out)
 # print(f'RMSPE = %.2f '%RMSPE+'%')
-plt.text(0.01,0.006, 'RMSPE = %.2f '%RMSPE+'%',fontweight='bold')
+# plt.text(0.01,0.006, 'RMSPE = %.2f '%RMSPE+'%',fontweight='bold')
 plt.xlabel('Time (s)')
 plt.ylabel(r'$\mu$mol H/mm')
 plt.title(f'In - Out = Accumulation')